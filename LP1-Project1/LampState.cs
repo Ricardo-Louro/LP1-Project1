645066968505c3ab54c56f8fef0a03c3157a5912
Pninja12 <74876860+Pninja12@users.noreply.github.com>
2023-04-10 15:18:07 +0100

    Enumeração corrigida

--- LP1-Project1/LampState.cs
+++ LP1-Project1/LampState.cs
@@ -3,15 +3,11 @@ using System;
 namespace LP1_Project1
 {
         [Flags]
-        enum lights
+    
+        public enum LampState
         {
             lamp1 = 0 << 0, // 0
             lamp2 = 0 << 0, // 0
             lamp3 = 0 << 0 // 0
-        }
-    
-        public enum LampState
-        {
-            
         }
 }
