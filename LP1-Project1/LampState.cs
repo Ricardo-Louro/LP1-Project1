f99fc0918a4ecd916543ff1765e4e277445b10b1
Pninja12 <74876860+Pninja12@users.noreply.github.com>
2023-04-10 15:01:14 +0100

    Adicionado Enumeração
    Foi adicionado enumeração ao projeto, nomeadamente as 3 lâmpadas.

--- LP1-Project1/LampState.cs
+++ LP1-Project1/LampState.cs
@@ -1,8 +1,17 @@
+using System;
+
 namespace LP1_Project1
 {
-    [Flags]
-    public enum LampState
-    {
-        
-    }
+        [Flags]
+        enum lights
+        {
+            lamp1 = 0 << 0, // 0
+            lamp2 = 0 << 0, // 0
+            lamp3 = 0 << 0 // 0
+        }
+    
+        public enum LampState
+        {
+            
+        }
 }
