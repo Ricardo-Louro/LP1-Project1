2a1f15c4d9ef93dd49622af187a2e0540908d1c0
Pninja12 <74876860+Pninja12@users.noreply.github.com>
2023-04-11 01:06:19 +0100

    Condição de Vitória
    Condição de Vitória adicionada
    Comentários adicionados e atualizados

--- LP1-Project1/LampState.cs
+++ LP1-Project1/LampState.cs
@@ -5,8 +5,8 @@ namespace LP1_Project1
         [Flags]
         public enum LampState
         {
-            Lamp1 = 1 << 0, // 1
-            Lamp2 = 1 << 1, // 2
-            Lamp3 = 1 << 2 // 4
+            Lamp1 = 1 << 0, // 0
+            Lamp2 = 1 << 1, // 0
+            Lamp3 = 1 << 2 // 0
         }
 }
